11774407d259aaa9596e860f2581d091a0ea2610
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2021-10-19 14:45:01 +0200

    Fjerner kode som ikke blir brukt.

--- src/main/kotlin/no/nav/personbruker/tms/innloggingsinfo/api/common/ConsumeEventException.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.personbruker.tms.innloggingsinfo.api.common
-
-import java.lang.Exception
-
-class ConsumeEventException(message: String, cause: Throwable) : Exception(message, cause)
